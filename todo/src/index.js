91b313df8440c4f91b1fd31ec967f8ed3c40178e
Konstantin Medvedev <medvedd07@gmail.com>
2018-12-13 16:41:22 +0300

    react-compon-class

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,22 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
 
-
-const App = () => {
-
-    const loginBox = <span>Log in Please</span>
-    return (
-        <div>
-            { loginBox }
-            <AppHeader />
-            <SearchPanel />
-            <TodoList />
-        </div>
-    );
-};
+import App from './components/app';
 
 ReactDOM.render(<App />,
-    document.getElementById('root'));
+  document.getElementById('root'));
