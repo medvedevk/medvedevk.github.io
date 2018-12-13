8a64d09ed8d73f56b35d139ca38e78ebac31a146
Konstantin Medvedev <medvedd07@gmail.com>
2018-12-13 21:17:34 +0300

    edit react setState

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -27,7 +27,7 @@ export default class TodoListItem extends React.Component {
     };
     render() {
 
-       const { label } =  this.props;
+       const { label, onDeleted } =  this.props;
        const { done, important } = this.state;
 
        let classNames = "todo-list-item";
@@ -57,6 +57,7 @@ export default class TodoListItem extends React.Component {
 
       <button type="button"
               className="btn btn-outline-danger btn-sm float-right"
+              onClick={ onDeleted }
 
       >
         <i className="fa fa-trash-o" />
